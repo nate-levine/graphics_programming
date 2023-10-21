cda3b5e6ac2541484385a70058a27db01bf35af9
nate-levine <n8levine11@gmail.com>
2023-10-21 12:51:04 -0400

    Replaced the sqaure witha cube and enabled depth-testing
    I replaced the square with a much more apt 3D cube. I also enabled the Z-buffer for depth testing

--- graphics_programming/src/main.cpp
+++ graphics_programming/src/main.cpp
@@ -51,17 +51,52 @@ int main()
     // Vertices data
     float vertices[] =
     {
-	/*position*/		    /*color*/           /*uv*/
-    -0.5f,  0.5f,  0.0f,	1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-	-0.5f, -0.5f,  0.0f,	0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-	 0.5f, -0.5f,  0.0f,	0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-	 0.5f,  0.5f,  0.0f,	1.0f, 0.0f, 1.0f,   1.0f, 1.0f,
+    	/*position*/		    /*color*/           /*uv*/
+        -0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 1.0f, // face 0
+        -0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+         0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 1.0f, // face 1
+         0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+         0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 1.0f, // face 2
+         0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 1.0f, // face 3
+        -0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 1.0f, // face 4
+        -0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+        -0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 1.0f, // face 5
+        -0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
     };
     // Indices data
     unsigned int indices[] =
     {
-	0, 1, 2,
-	0, 2, 3,
+    	 0,  1,  2, // face 0
+	     0,  2,  3,
+         4,  5,  6, // face 1
+         4,  6,  7,
+         8,  9, 10, // face 2
+         8, 10, 11,
+        12, 13, 14, // face 3
+        12, 14, 15,
+        16, 17, 18, // face 4
+        16, 18, 19,
+        20, 21, 22, // face 5
+        20, 22, 23,
     };
 
     // Create vertex array, vertex buffer and element buffers
@@ -126,8 +161,7 @@ int main()
     shader.use();
     // Model
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-20.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    model = glm::translate(model, glm::vec3(0.0f, 0.5f, 0.0f));
+    model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     shader.setMatrix4fv("model", model);
     // View
     glm::mat4 view = glm::mat4(1.0f);
@@ -137,9 +171,14 @@ int main()
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
     shader.setMatrix4fv("projection", projection);
 
+    // Enable the Z-buffer for depth testing
+    glEnable(GL_DEPTH_TEST);
+
     // While window is open
     while (!glfwWindowShouldClose(window))
     {
+        // Clear the Z-buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	    // Clear the color buffer
         glClear(GL_COLOR_BUFFER_BIT);
         
@@ -149,10 +188,14 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture);
 	    // Bind shader program
         shader.use();
+        model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(40.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::rotate(model, glm::radians((float)glfwGetTime() * 50.0f), glm::normalize(glm::vec3(1.0f, 0.0f, 1.0f)));
+        shader.setMatrix4fv("model", model);
 	    // Bind vertex array
 	    glBindVertexArray(VAO);
 	    // Draw vertices
-	    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
 	    // Swap the buffers
         glfwSwapBuffers(window);
